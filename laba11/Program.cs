8e06106b3c639b93c7e32e5ee7f000c538b330bc
linke <linke@DESKTOP-D9KFLQN>
2023-12-27 15:42:56 +0200

    Add project files.

---
+++ laba11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class DSAExample
+{
+    static void Main()
+    {
+        // Генерація ключів
+        using (var dsa = new DSACryptoServiceProvider())
+        {
+            // Підписання повідомлення
+            string message = "Hello, world!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            byte[] signature = SignData(data, dsa);
+
+            // Перевірка підпису
+            bool isValid = VerifyData(data, signature, dsa);
+
+            Console.WriteLine("Is signature valid? " + isValid);
+        }
+    }
+
+    static byte[] SignData(byte[] data, DSACryptoServiceProvider dsa)
+    {
+        // Генерація підпису
+        byte[] hash = SHA1.Create().ComputeHash(data);
+        byte[] signature = dsa.SignHash(hash, "SHA1");
+
+        return signature;
+    }
+
+    static bool VerifyData(byte[] data, byte[] signature, DSACryptoServiceProvider dsa)
+    {
+        // Верифікація підпису
+        byte[] hash = SHA1.Create().ComputeHash(data);
+        bool isValid = dsa.VerifyHash(hash, "SHA1", signature);
+
+        return isValid;
+    }
+}
